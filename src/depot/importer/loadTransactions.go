64b38cca962c2b5d1da156a5f7b7faa2ed170440
Ingo Fritz <ingofritz@outlook.com>
2025-03-23 18:31:14 +0000

    save pont

---
+++ src/depot/importer/loadTransactions.go
@@ -0,0 +1,13 @@
+package importer
+
+// func readFile(path string) []string {
+// 	lines := []string{}
+
+// 	return lines
+// }
+
+// Da LoadTransactions() den Rückgabetyp nicht kennt,
+// werde ich der Funktion eine andere Funktion mit übergeben. In der Funktion wird dann der Rückgabetyp definiert.
+func LoadTransactions() {
+
+}
